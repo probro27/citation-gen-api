f95e611145be8695e6e2926160b1b440940b335d
probro27 <prabhavkhera@hotmail.com>
2022-11-14 23:57:44 -0500

    tests added

--- model/test/apa.test.ts
+++ model/test/apa.test.ts
@@ -9,4 +9,9 @@ describe('testing apa citation', () => {
         const citation: Citation = new Apa(src, 7);
         expect(await citation.getCitation?.() == '');
     })
+    test('valid url test', async () => {
+        const src: Source = new Article("https://doi.org/10.1007/s10639-021-10650-9");
+        const citation: Citation = new Apa(src, 7);
+        expect(await citation.getCitation?.() == 'Alaattin Parlakkiliç (2021-07-12T00:00:00.000Z), Evaluating the effects of responsive design on the usability of academic websites in the pandemic - Education and Information Technologies, SpringerLink, https://doi.org/10.1007/s10639-021-10650-9');
+    })
 })
